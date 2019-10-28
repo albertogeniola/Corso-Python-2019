ce7778ccacb5ff11b10f6af0bc4517f84bf7ad66
Alberto Geniola <albertogeniola@gmail.com>
2019-10-28 21:51:30 +0100

    Added Lecture 3 exercises

---
+++ Lecure 3/1_number_divisibility.py
@@ -0,0 +1,19 @@
+"""
+Write a script that determines if one integer number is divisible by another.
+The two input numbers are provided via the following two variables:
+<a> and <b>.
+
+If the input_number_a is divisible by input_number_b, the program must print "Number <a> is
+divisible by <b> (quotient=<quotient>)". Otherwise, if that's not the case, the program should
+output "Number <a> is not divisible by <b> (remainder=<remainder>)"
+
+For instance, if a=10 and b=5, the program should print:
+Number 10 is divisible by 5 (quotient=2)
+
+Similarly, if a=11 and b=5, the expected output is:
+Number 11 is not divisible by 5 (remainder=1)
+
+So... would 99.603‬ be divisible by 17? Use the program you built to check it out!
+"""
+input_number_a = 10
+input_number_b = 5
---
+++ Lecure 3/2_list_length_comparer.py
@@ -0,0 +1,11 @@
+"""
+Write a script that, given two lists, determines which one contains the major number of elements and also
+tells how many elements the longer list has with respect of the shorter one.
+
+For instance, given the following lists...
+list_a = ['orange', 'blue', 'green', 'yellow']
+list_b = ['ford', 'toyota', 'fiat']
+The script should output: "list_a is longer as it contains 1 element more than list_b"
+"""
+list_a = ['orange', 'blue', 'green', 'yellow']
+list_b = ['ford', 'toyota', 'fiat']
