1d61c5ee0acd62b2e4ead4848f114ab8dfac9e89
Alberto Geniola <albertogeniola@gmail.com>
2019-10-29 21:31:43 +0100

    Soluzione dell'esercizio 2

--- Lecure 3/2_list_length_comparer.py
+++ Lecure 3/2_list_length_comparer.py
@@ -9,3 +9,17 @@ The script should output: "list_a is longer as it contains 1 element more than l
 """
 list_a = ['orange', 'blue', 'green', 'yellow']
 list_b = ['ford', 'toyota', 'fiat']
+
+el_a = len(list_a)
+el_b = len(list_b)
+
+print("La lista a contiene {} elementi, mentre la lista b ne contiente {}".format(el_a, el_b))
+if el_a > el_b:
+    delta = el_a - el_b
+    print("La lista a contiene {} elementi in più della lista b".format(delta))
+elif el_a == el_b:
+    delta = 0
+    print("La lista a contiene lo stesso numero di elementi della lista b")
+else:
+    delta = el_b - el_a
+    print("La lista b contiene {} elementi in più della lista a".format(delta))
