d3f104e9c966201c1c8711596b46d260885090d1
Alberto Geniola <albertogeniola@gmail.com>
2019-11-04 22:14:36 +0100

    Rinominata la cartella per la lezione 3 Aggiunto esercizio Lezione 4

---
+++ Lecture 4/1_remove_duplicates.py
@@ -0,0 +1,19 @@
+"""
+Given a list of elements, remove all the duplicates and print all the elements in a sorted manner
+
+Hint!
+There are two ways for doing so... one is to use low memory and much CPU,
+the other way around is to use more memory and less CPU.
+"""
+
+data = [48, 0, 10, 22, 5, 37, 22, 50, 19, 32, 42, 21, 5, 4, 16, 44, 42, 21, 7, 28, 43, 47, 33, 13, 8, 50, 29, 35, 12,
+        39, 44, 36, 46, 45, 26, 13, 50, 37, 1, 31, 30, 6, 18, 38, 14, 31, 2, 25, 1, 41, 1, 20, 43, 50, 28, 30, 25, 33,
+        4, 11, 36, 49, 24, 7, 29, 32, 2, 10, 11, 2, 0, 43, 31, 3, 42, 39, 39, 28, 29, 8, 4, 6, 5, 5, 13, 49, 16, 25, 1,
+        32, 24, 27, 48, 27, 9, 22, 14, 5, 10, 0]
+
+
+# Attempt 1: CPU-Iterative way
+
+# Attempt 2: Using another data structure as helper
+
+# Attempt 3: Quickest way
